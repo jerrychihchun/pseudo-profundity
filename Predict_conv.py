39da6af20aec3a3be9a78899f650568445302456
jerrychihchun <jerrychihchun@gmail.com>
2019-10-16 15:16:16 +0200

    Example model and tokenizer for command line use

---
+++ Predict_conv.py
@@ -0,0 +1,29 @@
+import json
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from keras_preprocessing.text import tokenizer_from_json
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+model_conv = tf.keras.models.load_model('1DConv_model.h5')
+
+tokenizer = Tokenizer()
+with open('tokenizer.json') as f:
+    data = json.load(f)
+    tokenizer = tokenizer_from_json(data)
+
+max_length = 100
+trunc_type='post'
+padding_type='post'
+
+if __name__== "__main__" :
+	predict = True
+	while predict:
+	    text = input('Enter a text to detect pseudo-profundity (q to quit):')
+	    if text == 'q':
+	    	break
+	    sequences = tokenizer.texts_to_sequences([text])
+	    padded = pad_sequences(sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+	    print(model_conv.predict(padded))
+
+
+
